4f9992a8e1bc111fd535ee230d64157164f203da
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-15 20:21:08 +0300

    Separate salary prediction for code consistency

---
+++ utils.py
@@ -0,0 +1,15 @@
+from typing import Union
+
+
+def predict_salary(salary_from: int, salary_to: int) -> Union[int, float, None]:
+    if not (salary_from or salary_to):
+        return
+    if not salary_to:
+        return salary_from * 1.2
+    if not salary_from:
+        return salary_to * 0.8
+    return (salary_from + salary_to) / 2
+
+
+if __name__ == "__main__":
+    print(predict_salary(None, None))
