5310bb8eb45798ec6429e911a06858af8662a207
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-15 08:28:39 +0300

    Add vacancies filters

--- hh.py
+++ hh.py
@@ -2,13 +2,18 @@ import requests
 from pprint import pprint
 
 HH_BASE_API = "https://api.hh.ru"
-DEVELOPER_ROLE_ID = 96
 
 
-def get_vacacnies(base_url, endpoint, role_id):
+def get_vacacnies(
+    base_url: str, endpoint: str, role_id: int, area_id: int, period: int
+):
 
     url = f"{base_url}{endpoint}"
-    params = {"professional_role": role_id}
+    params = {
+        "professional_role": role_id,
+        "area": area_id,
+        "period": period,
+    }
 
     response = requests.get(url, params)
     response.raise_for_status()
@@ -16,4 +21,4 @@ def get_vacacnies(base_url, endpoint, role_id):
     return response.json()
 
 
-pprint(get_vacacnies(HH_BASE_API, "/vacancies", DEVELOPER_ROLE_ID))
+pprint(get_vacacnies(HH_BASE_API, "/vacancies", 96, 1, 30))
