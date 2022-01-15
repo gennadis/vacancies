b0bb0487c12f8e138b0f5cbe1c5509848a741ea5
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-15 08:53:15 +0300

    Get python job salaries

--- hh.py
+++ hh.py
@@ -2,10 +2,11 @@ import requests
 from pprint import pprint
 
 HH_BASE_API = "https://api.hh.ru"
+PROG_LANGUAGES = ["JavaScript", "Python", "Go", "Java", "Kotlin", "C++", "PHP", "Swift"]
 
 
 def get_vacacnies(
-    base_url: str, endpoint: str, role_id: int, area_id: int, period: int
+    base_url: str, endpoint: str, role_id: int, area_id: int, period: int, text: str
 ):
 
     url = f"{base_url}{endpoint}"
@@ -13,6 +14,7 @@ def get_vacacnies(
         "professional_role": role_id,
         "area": area_id,
         "period": period,
+        "text": text,
     }
 
     response = requests.get(url, params)
@@ -21,4 +23,11 @@ def get_vacacnies(
     return response.json()
 
 
-pprint(get_vacacnies(HH_BASE_API, "/vacancies", 96, 1, 30))
+def main():
+    python_jobs = get_vacacnies(HH_BASE_API, "/vacancies", 96, 1, 30, "ptyhon")["items"]
+    for job in python_jobs:
+        print(job["salary"])
+
+
+if __name__ == "__main__":
+    main()
