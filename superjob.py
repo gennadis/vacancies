20f7c64df186d611c22472eb5199f8becb2b593a
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-15 20:22:20 +0300

    Add Superjob salary predictions

--- superjob.py
+++ superjob.py
@@ -1,9 +1,11 @@
 import os
+import requests
 from pprint import pprint
+from typing import Optional
 
 from dotenv import load_dotenv
 
-import requests
+from utils import predict_salary
 
 SUPERJOB_API_URL = "https://api.superjob.ru/2.0"
 ENDPOINT = "/vacancies"
@@ -35,6 +37,17 @@ def get_vacancies(
     return vacancies
 
 
+def predict_rub_salary_for_sj(vacancie: dict) -> Optional[int]:
+    salary_from: int = vacancie.get("payment_from")
+    salary_to: int = vacancie.get("payment_to")
+
+    if vacancie.get("currency") != "rub":
+        return
+    if salary_from == salary_to == 0:
+        return
+    return int(predict_salary(salary_from, salary_to))
+
+
 def main():
     load_dotenv()
     superjob_token = os.getenv("SUPERJOB_TOKEN")
@@ -43,13 +56,18 @@ def main():
         base_url=SUPERJOB_API_URL,
         endpoint=ENDPOINT,
         token=superjob_token,
-        town_id=4,
-        profession_id=48,
+        town_id=4,  # "Москва"
+        profession_id=48,  # "Разработка, программирование"
         keyword="программист",
     )
 
     for vacancie in vacancies:
-        print(vacancie["profession"], vacancie["town"]["title"])
+        print(
+            vacancie["profession"],
+            vacancie["town"]["title"],
+            predict_rub_salary_for_sj(vacancie),
+            sep=", ",
+        )
 
 
 if __name__ == "__main__":
