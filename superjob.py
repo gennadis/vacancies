2986aab2acdc1ed28127ab3c3f8f8051c9ce47b5
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-15 19:35:12 +0300

    Add search filters for vacancies

--- superjob.py
+++ superjob.py
@@ -9,14 +9,26 @@ SUPERJOB_API_URL = "https://api.superjob.ru/2.0"
 ENDPOINT = "/vacancies"
 
 
-def get_vacancies(base_url: str, endpoint: str, token: str) -> list[dict]:
+def get_vacancies(
+    base_url: str,
+    endpoint: str,
+    token: str,
+    town_id: int,
+    profession_id: int,
+    keyword: str,
+) -> list[dict]:
 
     url = f"{base_url}{endpoint}"
     headers = {
         "X-Api-App-Id": token,
     }
+    params = {
+        "town": town_id,
+        "catalogues": profession_id,
+        "keyword": keyword,
+    }
 
-    response = requests.get(url=url, headers=headers)
+    response = requests.get(url=url, headers=headers, params=params)
     response.raise_for_status()
 
     vacancies = response.json()["objects"]
@@ -27,10 +39,17 @@ def main():
     load_dotenv()
     superjob_token = os.getenv("SUPERJOB_TOKEN")
 
-    vacancies = get_vacancies(SUPERJOB_API_URL, ENDPOINT, superjob_token)
+    vacancies = get_vacancies(
+        base_url=SUPERJOB_API_URL,
+        endpoint=ENDPOINT,
+        token=superjob_token,
+        town_id=4,
+        profession_id=48,
+        keyword="программист",
+    )
 
     for vacancie in vacancies:
-        print(vacancie["profession"])
+        print(vacancie["profession"], vacancie["town"]["title"])
 
 
 if __name__ == "__main__":
