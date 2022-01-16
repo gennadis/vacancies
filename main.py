c55c80a886dde5ae419ccc01e5799635e385e61b
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-16 18:05:26 +0300

    Print nice vacancy statistics tables

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
-from pprint import pprint
 
 from dotenv import load_dotenv
+from terminaltables import SingleTable
 
 from hh import get_vacancies_hh, predict_rub_salary_hh
 from superjob import get_vacancies_sj, predict_rub_salary_sj
-from utils import get_vacancies_stats
+from utils import get_vacancies_stats, prepare_table
 
 HH_API_BASE_URL = "https://api.hh.ru"
 HH_VACANCIES_ENDPOINT = "/vacancies"
@@ -26,40 +26,61 @@ PROGRAMMING_LANGUAGES = [
 ]
 
 
-def main():
-
-    load_dotenv()
-    superjob_token = os.getenv("SUPERJOB_TOKEN")
-
+def get_hh_total_stats(languages: list):
     hh_total_stats = {}
-    for language in PROGRAMMING_LANGUAGES:
+    for language in languages:
         vacancies = get_vacancies_hh(
             base_url=HH_API_BASE_URL,
             endpoint=HH_VACANCIES_ENDPOINT,
             role_id=96,  # Developer
             area_id=1,  # Moscow
-            period=30,
+            period=30,  # last month
             text=language,
             per_page=100,
         )
         hh_total_stats[language] = get_vacancies_stats(vacancies, predict_rub_salary_hh)
-    pprint(hh_total_stats)
 
-    print("-" * 35)
+    return hh_total_stats
 
+
+def get_sj_total_stats(languages: list, token: str):
     sj_total_stats = {}
-    for language in PROGRAMMING_LANGUAGES:
+    for language in languages:
         vacancies = get_vacancies_sj(
             base_url=SJ_API_BASE_URL,
             endpoint=SJ_VACANCIES_ENDPOINT,
-            token=superjob_token,
+            token=token,
             town_id=4,  # Moscow
             profession_id=48,  # Developer
             keyword=language,
             per_page=100,
         )
         sj_total_stats[language] = get_vacancies_stats(vacancies, predict_rub_salary_sj)
-    pprint(sj_total_stats)
+    return sj_total_stats
+
+
+def main():
+    load_dotenv()
+    superjob_token = os.getenv("SUPERJOB_TOKEN")
+
+    table_headers = [
+        "Язык программирования",
+        "Вакансий найдено",
+        "Вакансий обработано",
+        "Средняя зарплата",
+    ]
+
+    hh_total_stats = get_hh_total_stats(PROGRAMMING_LANGUAGES)
+    hh_table_data = prepare_table(table_headers=table_headers, content=hh_total_stats)
+    hh_table = SingleTable(hh_table_data)
+    hh_table.title = "HeadHunter"
+    print(hh_table.table)
+
+    sj_total_stats = get_sj_total_stats(PROGRAMMING_LANGUAGES, superjob_token)
+    sj_table_data = prepare_table(table_headers=table_headers, content=sj_total_stats)
+    sj_table = SingleTable(sj_table_data)
+    sj_table.title = "SuperJob"
+    print(sj_table.table)
 
 
 if __name__ == "__main__":
