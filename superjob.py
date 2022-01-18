29e58336df887fac78a7afc136516ab17e4ce2e4
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-18 11:03:40 +0300

    Move SuperJob code from main module

--- superjob.py
+++ superjob.py
@@ -1,7 +1,11 @@
 import requests
 from typing import Optional
 
-from utils import predict_salary
+from utils import predict_salary, collect_vacancies_stats
+
+
+SJ_API_BASE_URL = "https://api.superjob.ru/2.0"
+SJ_VACANCIES_ENDPOINT = "/vacancies"
 
 
 def fetch_vacancies_from_sj(
@@ -60,3 +64,20 @@ def predict_rub_salary_sj(vacancy: dict) -> Optional[int]:
     if salary_from == salary_to == 0:
         return
     return int(predict_salary(salary_from, salary_to))
+
+
+def collect_stats_from_sj(languages: list, token: str) -> dict:
+    """Get SuperJob vacancies stats for programming languages."""
+    sj_stats = {}
+    for language in languages:
+        vacancies = fetch_vacancies_from_sj(
+            base_url=SJ_API_BASE_URL,
+            endpoint=SJ_VACANCIES_ENDPOINT,
+            token=token,
+            town_id=4,  # Moscow
+            profession_id=48,  # Developer
+            keyword=language,
+            per_page=100,
+        )
+        sj_stats[language] = collect_vacancies_stats(vacancies, predict_rub_salary_sj)
+    return sj_stats
