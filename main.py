cc73424e89ec565e3ddc6ba7408c03b444d997c2
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-18 11:07:27 +0300

    Clean from HH and SJ code

--- main.py
+++ main.py
@@ -2,50 +2,9 @@ import os
 
 from dotenv import load_dotenv
 
-from hh import fetch_vacancies_from_hh, predict_rub_salary_hh
-from superjob import fetch_vacancies_from_sj, predict_rub_salary_sj
-from utils import collect_vacancies_stats, create_table
-
-HH_API_BASE_URL = "https://api.hh.ru"
-HH_VACANCIES_ENDPOINT = "/vacancies"
-
-SJ_API_BASE_URL = "https://api.superjob.ru/2.0"
-SJ_VACANCIES_ENDPOINT = "/vacancies"
-
-
-def collect_stats_from_hh(languages: list) -> dict:
-    """Get HeadHunter vacancies stats for programming languages."""
-    hh_stats = {}
-    for language in languages:
-        vacancies = fetch_vacancies_from_hh(
-            base_url=HH_API_BASE_URL,
-            endpoint=HH_VACANCIES_ENDPOINT,
-            role_id=96,  # Developer
-            area_id=1,  # Moscow
-            period=30,  # last month
-            text=language,
-            per_page=100,
-        )
-        hh_stats[language] = collect_vacancies_stats(vacancies, predict_rub_salary_hh)
-
-    return hh_stats
-
-
-def collect_stats_from_sj(languages: list, token: str) -> dict:
-    """Get SuperJob vacancies stats for programming languages."""
-    sj_stats = {}
-    for language in languages:
-        vacancies = fetch_vacancies_from_sj(
-            base_url=SJ_API_BASE_URL,
-            endpoint=SJ_VACANCIES_ENDPOINT,
-            token=token,
-            town_id=4,  # Moscow
-            profession_id=48,  # Developer
-            keyword=language,
-            per_page=100,
-        )
-        sj_stats[language] = collect_vacancies_stats(vacancies, predict_rub_salary_sj)
-    return sj_stats
+from hh import collect_stats_from_hh
+from superjob import collect_stats_from_sj
+from utils import create_table
 
 
 def main():
